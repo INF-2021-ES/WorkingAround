036413ec2382b41db73f939be2849f31ef75446a
Ricardo Sousa <a037322@ismai.pt>
2021-06-06 18:46:51 +0100

    user display info on index

--- working-around/resources/views/user/index.blade.php
+++ working-around/resources/views/user/index.blade.php
@@ -1,40 +1,36 @@
 @extends('layouts.app')
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>User Details</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>User Details</h2>
+            </div>
         </div>
+    </div>
+    <div class="col">
+        <br/>
+        <a class="btn btn-primary" href="{{route('user.edit', $user->id)}}">Edit</a>
+        <br/>
+        <br/>
+        <table class="table table-bordered">
+            <tr>
+                <th>Name</th>
+                <td>{{$user->name}}</td>
+            </tr>
+            <tr>
+                <th>E-Mail</th>
+                <td>{{$user->email}}</td>
+            </tr>
+            <tr>
+                <th>Address</th>
+                <td>{{$user->address}}</td>
+            </tr>
+            <tr>
+                <th>Roles</th>
+                <td>{{$user->roles}}</td>
+            </tr>
+        </table>
     </div>
 </div>
-
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <b>Name</b>
-            {{$user->name}}
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <b>E-Mail</b>
-            {{$user->email}}
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <b>Address</b>
-            {{$user->address}}
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <b>Roles</b>
-            {{$user->roles}}
-        </div>
-    </div>
-</div> 
 @endsection
