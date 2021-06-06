4457bd8665941a750a6784c7d035a27ac719e00b
Miguel Azevedo <mra.azevedo@hotmail.com>
2021-06-06 21:07:35 +0100

    Update index.blade.php

--- working-around/resources/views/user/index.blade.php
+++ working-around/resources/views/user/index.blade.php
@@ -19,6 +19,15 @@
     <div class="col">
         <br/>
         <a class="btn btn-primary" href="{{route('user.edit', $user->id)}}">Edit</a>
+        @can('create-service') <!-- Worker -->
+            <a class="btn btn-danger"href="{{route('user.removeWorker')}}">Delete</a> 
+        @endcan
+        @can('reserve-service') <!-- Client -->
+            <a class="btn btn-danger"href="{{route('user.removeClient')}}">Delete</a> 
+        @endcan
+        @can('create-category') <!-- Admin -->
+            <a class="btn btn-danger"href="{{route('user.removeClient')}}">Delete</a> <!-- Client methods works in admin -->
+        @endcan
         <br/>
         <br/>
         <table class="table table-bordered">
