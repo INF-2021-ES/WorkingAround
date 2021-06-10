1cdd5667c8e22b8988675c9fd18056afdadc6f98
Francisco970-hub <77981948+Francisco970-hub@users.noreply.github.com>
2021-06-10 18:54:51 +0100

    arranjos css

--- working-around/resources/views/auth/register.blade.php
+++ working-around/resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('pageTitle', 'Register')
 @section('content')
-<div class="container">
+<div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -95,4 +95,14 @@
         </div>
     </div>
 </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 @endsection
