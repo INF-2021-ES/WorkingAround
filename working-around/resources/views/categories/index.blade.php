951ef4c782a9c231d1cb9824158f85c5076b3fe0
Ricardo Sousa <a037322@ismai.pt>
2021-06-11 12:33:46 +0100

    resolvido
    - espaco em branco por baixo do footer em algumas paginas (min-vh-100)
    - clip do container com o footer (min-vh-100)

--- working-around/resources/views/categories/index.blade.php
+++ working-around/resources/views/categories/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('pageTitle', 'Categories')
 @section('content')
-    <div class="container my-4 vh-100">
+    <div class="container my-4">
         <div class="row row-cols-1 row-cols-md-3">
             @foreach ($categories as $category)
                 <div class="col mb-4">
